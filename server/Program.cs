64b6bb2183205171dcf4df885938c0b06bed84e2
mathum001 <mattias.hummer@hotmail.com>
2024-02-06 11:41:22 +0100

    serverstart func
    serverstart func

--- server/Program.cs
+++ server/Program.cs
@@ -18,4 +18,45 @@ class Program
     {
         StartServer();
     }
+
+    static void StartServer()
+    {
+        TcpListener server = null;
+        try
+        {
+            // Ange IP-adressen och porten som servern ska lyssna på
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            int port = 8080;
+
+            // Skapa en TCP-listener på den angivna IP-adressen och porten
+            server = new TcpListener(ipAddress, port);
+
+            // Starta lyssnaren
+            server.Start();
+            Console.WriteLine("Servern är igång och lyssnar på port " + port);
+
+
+
+            while (true)
+            {
+                // Vänta på en anslutning från en klient
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("En klient har anslutit.");
+
+                //ny tråd för separata klienter
+                Thread clientThread = new Thread(HandleClient);
+                clientThread.Start(client);
+
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+        finally
+        {
+            // Stäng servern
+            server?.Stop();
+        }
+    }
 }
