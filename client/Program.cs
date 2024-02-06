9b4f77aed5ca0f159ab79b2aebbdd2f389fc1a66
juliebefana <marcusjuliahiller@gmail.com>
2024-02-06 11:53:10 +0100

    fixed StartClient

--- client/Program.cs
+++ client/Program.cs
@@ -7,9 +7,54 @@ namespace client;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Client!");
-        StartClient();
-    }
+    static void StartClient()
+        {
+            try
+            {
+                TcpClient client = new TcpClient("127.0.0.1", 8080);
+                Console.WriteLine("Connected to the server.");
+
+                NetworkStream stream = client.GetStream();
+
+                Console.WriteLine("Enter 'register' or 'login':");
+                string userInput = Console.ReadLine();
+
+                if (userInput?.ToLower() == "register")
+                {
+                    RegisterUser(stream);
+                }
+                else if (userInput?.ToLower() == "login")
+                {
+                    LoginUser(stream);
+                }
+
+                //Ny tråd som lyssnar på medd från servern
+                Thread receiveThread = new Thread(() => ReceiveMessages(stream));
+                receiveThread.Start();
+
+                while (true)
+                {
+                    Console.WriteLine("Enter 'send' to broadcast or 'private' for a private message:");
+                    string command = Console.ReadLine();
+
+                    if (command?.ToLower() == "send")
+                    {
+                        BroadcastMessage(stream);
+                    }
+                    else if (command?.ToLower() == "private")
+                    {
+                        SendPrivateMessage(stream);
+                    }
+                    else
+                    {
+                        client.Close();
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
 }
