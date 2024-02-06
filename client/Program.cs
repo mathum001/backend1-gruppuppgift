38488fa2018e7b8037bb2fdad9eaac4230377519
juliebefana <marcusjuliahiller@gmail.com>
2024-02-06 12:03:46 +0100

    SendPrivateMessages in Client

--- client/Program.cs
+++ client/Program.cs
@@ -12,4 +12,17 @@ class Program
         Console.WriteLine("Hello Client!");
         StartClient();
     }
+
+    static void SendPrivateMessage(NetworkStream stream)
+        {
+            Console.WriteLine("Enter recipient's username:");
+            string recipient = Console.ReadLine();
+
+            Console.WriteLine("Enter your private message:");
+            string message = Console.ReadLine();
+
+            // Send private message to the server
+            string dataToSend = $"sendPrivate {recipient} {message}";
+            SendData(stream, dataToSend);
+        }
 }
