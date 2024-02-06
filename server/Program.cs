f817d91f18d3f662701934825f556be56cea6de3
mathum001 <mattias.hummer@hotmail.com>
2024-02-06 12:10:41 +0100

    server-SendPM
    as

--- server/Program.cs
+++ server/Program.cs
@@ -18,4 +18,29 @@ class Program
     {
         StartServer();
     }
+
+    static void SendPrivateMessage(string parameters, NetworkStream senderStream)
+    {
+        string[] data = parameters.Split(" ");
+        if (data.Length < 2)
+        {
+            Console.WriteLine("Incorrect private message format.");
+            return;
+        }
+        string sender = GetUsernameByStream(senderStream);
+        string recipient = data[0];
+        string message = sender + ": " + parameters.Substring(recipient.Length).Trim();
+
+        if (userStreams.TryGetValue(recipient, out NetworkStream recipientStream))
+        {
+            byte[] dataToSend = Encoding.ASCII.GetBytes(message);
+            recipientStream.Write(dataToSend, 0, dataToSend.Length);
+        }
+        else
+        {
+            Console.WriteLine($"User '{recipient}' not found or offline.");
+        }
+
+        AddSingleMessageToDB(recipientStream, message);
+    }
 }
