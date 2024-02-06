ef84203fce3e0e0f39be322b24b0848b13e80212
juliebefana <92132933+juliebefana@users.noreply.github.com>
2024-02-06 12:48:51 +0100

    ReceiveMessages

--- client/Program.cs
+++ client/Program.cs
@@ -12,4 +12,25 @@ class Program
         Console.WriteLine("Hello Client!");
         StartClient();
     }
+
+    static void ReceiveMessages(NetworkStream stream)
+        {
+            try
+            {
+                byte[] buffer = new byte[1024];
+                while (true)
+                {
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    if (bytesRead > 0)
+                    {
+                        string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                        Console.WriteLine(message);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error receiving message from server: " + e.Message);
+            }
+        }
 }
