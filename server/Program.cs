519764cfcb377f30b767cd90324299d7755d6f56
mathum001 <mattias.hummer@hotmail.com>
2024-02-06 12:12:40 +0100

    server-FetchMongoUser
    asd

--- server/Program.cs
+++ server/Program.cs
@@ -17,5 +17,33 @@ class Program
     static void Main(string[] args)
     {
         StartServer();
+
+        static IMongoCollection<User> FetchMongoUser()
+    {
+        const string newpass = "KokxLPCVbH0hKrp2";
+        string connectionUri = "mongodb+srv://mattiashummer:" + newpass + "@cluster0.y5yh9uz.mongodb.net/?retryWrites=true&w=majority";
+
+        var settings = MongoClientSettings.FromConnectionString(connectionUri);
+        // Set the ServerApi field of the settings object to Stable API version 1
+        settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+        // Create a new client and connect to the server
+        var client = new MongoClient(settings);
+        // Send a ping to confirm a successful connection
+        try
+        {
+            var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+            Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+        // anslut till databasen
+        var database = client.GetDatabase("testing");
+        //anslut till kollektion
+        IMongoCollection<User> collection = database.GetCollection<User>("users");
+
+        return collection;
+    }
     }
 }
